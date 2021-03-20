4777efd01ba56d6860e6ecd62096cc81ab6e96fa
Luke <luke92brown@gmail.com>
2021-03-20 14:14:05 -0500

    changed variable names and added comments

--- yt_irc.py
+++ yt_irc.py
@@ -65,7 +65,10 @@ for server_log_dir in os.listdir(input_path):
             print(chatroom_count, chatroom_path)
             chatroom_count += 1
 
+
+#########################
 # ask user for exclusions
+#########################
 while True:
     user_choice = input("\nEnter index to exclude from playlist (-1 when finished): ")
 
@@ -79,27 +82,36 @@ while True:
     for count, f in enumerate(chatroom_paths):
         print(count, f)
 
-pattern = re.compile("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?")
 
+#############
+# search logs
+#############
+pattern = re.compile("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?")
 youtube_ids = []
 
 for chatroom_path in chatroom_paths:
+
+    # find all youtube urls
     with open(chatroom_path, 'r') as fp:
         chatroom_log_text = fp.read()
-        matches = re.findall(pattern, chatroom_log_text)
+        matches = re.findall(pattern, chatroom_log_text) # returns a tuple 
+
+        # only keep first element of tuple containing youtube_id, discard the rest
         for match in matches:
             youtube_id = match[0]
             if youtube_id:
                 youtube_ids.append(youtube_id)
 
+
+#############################################################
+# change list of IDs into usable links and output to terminal
+#############################################################
 list_of_lists = [[]] * int((len(youtube_ids) / 50))
 
-# print(list_of_lists)
+# break large list into multiple smaller lists containing 50 youtube ids
 list_of_lists = chunks(youtube_ids, 50)
 
-for list in list_of_lists:
-    csv_youtube_id = ','.join(list)
-    playlist_url = "https://www.youtube.com/watch_videos?video_ids=" + csv_youtube_id
-    print(playlist_url)
-
-
+for fifty_youtube_ids in list_of_lists:
+    comma_seperated_youtube_ids = ','.join(fifty_youtube_ids)
+    playlist_url = "https://www.youtube.com/watch_videos?video_ids=" + comma_seperated_youtube_ids
+    print(playlist_url, '\n')
